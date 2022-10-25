b7af6469bbd90764a79bfbc102dc9be30fb28250
FelixHertz <84127057+FelixHertz@users.noreply.github.com>
2022-10-25 16:27:53 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -27,7 +27,7 @@ def add_data_other(wort, gebrauch):
 
 words = ['a']
 
-with open('deutsch.txt', "w") as fp:
+with open('deutsch.txt', mode='r', encoding='utf-8') as fp:
     contents = fp.read().splitlines()
     for i in contents:
         words.append(i)
