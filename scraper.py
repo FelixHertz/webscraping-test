a8b2fb55f1e38c371f1dd666a6525e4c8e533531
FelixHertz <84127057+FelixHertz@users.noreply.github.com>
2022-10-25 15:49:32 +0200

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -1,22 +1,63 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-import os
+from bs4 import BeautifulSoup
+import requests
+import re
+import mysql.connector
 
 
+mydb = mysql.connector.connect(host="h1.host.filess.io", user="dudenv2_bystreamwe", passwd="239848e4ad651c7b21981c6cd1fea4bf6ec5c444", database="dudenv2_bystreamwe", port="3307")
+mycursor = mydb.cursor()
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN") 
-chrome_options.add_argument ("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
+def add_data(wort, gebrauch):
+    try:
+        sql = "INSERT INTO wort_erg (wort, gebrauch) VALUES (%s, %s)"
+        val = (wort, gebrauch)
+        mycursor.execute(sql, val)
+        mydb.commit()
+    except:
+                print("Error")
+
+def add_data_other(wort, gebrauch):
+    try:
+        sql = "INSERT INTO other (wort, gebrauch) VALUES (%s, %s)"
+        val = (wort, gebrauch)
+        mycursor.execute(sql, val)
+        mydb.commit()
+    except:
+                print("Error")
+
+words = ['a']
+
+with open('deutsch.txt') as fp:
+    contents = fp.read().splitlines()
+    for i in contents:
+        words.append(i)
 
 
+header = {"User-Agent":"Custom"}
 
-driver = webdriver.Chrome(service=Service(os.environ.get("CHROMEDRIVER_PATH")), options=chrome_options)
-##driver = webdriver.Chrome(service=Service('./chromedriver'), options=chrome_options)
-driver.get("http://www.google.com/")
-print(driver.page_source)
-        
+def scrape():
+    for entry in words:
+        try:
+            url = f"https://www.duden.de/rechtschreibung/{entry}"
+
+            print(entry)
+
+            result = requests.get(url, headers=header)
+            doc = BeautifulSoup(result.text, "html.parser")
+            
+            search = doc.find(text=re.compile('Gebrauch:')).parent.find_next('dd')
+            extract = search.text
+            print(extract)
+
+            if extract == 'bildungssprachlich':
+                add_data(entry, 'bildungssprachlich')
+            if extract == 'gehoben':
+                add_data(entry, 'gehoben')
+            if extract != 'bildungssprachlich':
+                if extract != 'gehoben':
+                    add_data_other(entry, extract)
+
+        except:
+            print("")
+scrape()
+
