188fdf19955691f733d51277b7dba3ccc245175c
FelixHertz <84127057+FelixHertz@users.noreply.github.com>
2022-10-25 12:43:51 +0200

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -10,8 +10,8 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument ("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-##chrome_options.set_capability("browserVersion", "85.0.4183.83")
-##chrome_options.set_capability("platformName", "linux")
+chrome_options.set_capability("browserVersion", "106.0.5249.61")
+chrome_options.set_capability("platformName", "linux")
 
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
