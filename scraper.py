69b04a86da9b93cf8e83aa80ae59945ec092e017
FelixHertz <84127057+FelixHertz@users.noreply.github.com>
2022-10-25 13:16:27 +0200

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -3,19 +3,20 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+import os
 
 
 
 chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN") 
 chrome_options.add_argument ("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-##chrome_options.set_capability("browserVersion", "106.0.5249.61")
-##chrome_options.set_capability("platformName", "linux")
 
 
-##driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-driver = webdriver.Chrome(service=Service('./chromedriver.exe'), options=chrome_options)
+
+driver = webdriver.Chrome(service=Service(os.environ.get("CHROMEDRIVER_PATH")), options=chrome_options)
+##driver = webdriver.Chrome(service=Service('./chromedriver'), options=chrome_options)
 driver.get("http://www.google.com/")
 print(driver.page_source)
         
